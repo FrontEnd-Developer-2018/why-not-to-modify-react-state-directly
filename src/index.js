c5cf7d02259ba5fc3f3198d41acc09f67a62eff6
ollydbg-1 <44168374+ollydbg-1@users.noreply.github.com>
2021-12-11 20:30:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import HelloPureComponent from "./HelloPureComponent";
 import "./style.css";
 /*
 
-As you may already know, a common way to tune a React component for performance is to make it “pure,” which causes it to only re-render when its props change (instead of every time its parent re-renders). This can be done automatically by extending React.PureComponent instead of React.Component, or manually by implementing the shouldComponentUpdate lifecycle method to compare nextProps with current props. If the props look the same, it skips the render, and saves some time.
+As you may already know, a common way to tune a React component for performance is to make it “pure,” which causes it to only re-render 
+when its props change (instead of every time its parent re-renders). This can be done automatically by extending React.PureComponent instead of React.Component,
+or manually by implementing the shouldComponentUpdate lifecycle method to compare nextProps with current props. If the props look the same, it skips the render,
+and saves some time.
 
 
 Immutability Is Important for PureComponents
-By default, React components (both the function type and the class type, if it extends React.Component) will re-render whenever their parent re-renders, or whenever you change their state with setState.
-An easy way to optimize a React component for performance is to make it a class, and make it extend React.PureComponent instead of React.Component. This way, the component will only re-render if its state is changed or if its props have changed. It will no longer mindlessly re-render every single time its parent re-renders; it will ONLY re-render if one of its props has changed since the last render.
+By default, React components (both the function type and the class type, if it extends React.Component) will re-render whenever their parent re-renders, 
+or whenever you change their state with setState.
+An easy way to optimize a React component for performance is to make it a class, and make it extend React.PureComponent instead of React.Component. 
+This way, the component will only re-render if its state is changed or if its props have changed. It will no longer mindlessly re-render every single time its parent re-renders; it will ONLY re-render if one of its props has changed since the last render.
 
 */
 class App extends Component {
